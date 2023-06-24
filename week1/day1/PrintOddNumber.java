cf82d7d16918e737a293c5a74616515af15492e0
vishvaSelenium12 <137630095+vishvaSelenium12@users.noreply.github.com>
2023-06-24 21:01:45 +0530

    Add files via upload
    week1 day 1 files uploaded

---
+++ week1/day1/PrintOddNumber.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class PrintOddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0;i<=20;i++) {
+			if(i%2!=0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
