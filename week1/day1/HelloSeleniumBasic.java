66604f1b83073549b819d8ccd5c368707b452674
vishvaSelenium12 <137630095+vishvaSelenium12@users.noreply.github.com>
2023-06-25 18:47:07 +0530

    Add files via upload
    week1 day 1 missing file upload

---
+++ week1/day1/HelloSeleniumBasic.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloSeleniumBasic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my second code");
+	}
+
+}
